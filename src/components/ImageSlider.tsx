d61168a823b46585eb932c43cde38afc23ffebe8
Dmitry <dmitry27zh@gmail.com>
2024-01-04 10:20:04 +0500

    Toggle photo

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -8,14 +8,32 @@ type ImageSliderProps = {
 
 const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
   const [imageIndex, setImageIndex] = useState(0)
+  const showPrevImage = () => {
+    setImageIndex((prevState) => {
+      if (prevState === 0) {
+        return imageUrls.length - 1
+      } else {
+        return imageIndex - 1
+      }
+    })
+  }
+  const showNextImage = () => {
+    setImageIndex((prevState) => {
+      if (prevState === imageUrls.length - 1) {
+        return 0
+      } else {
+        return imageIndex + 1
+      }
+    })
+  }
 
   return (
     <div style={{ position: 'relative', width: '100%', height: '100%' }}>
       <img className="img-slider-img" src={imageUrls[imageIndex]} alt="Main image" />
-      <button className="img-slider-btn" style={{ left: 0 }}>
+      <button className="img-slider-btn" style={{ left: 0 }} onClick={showPrevImage}>
         <ArrowBigLeft />
       </button>
-      <button className="img-slider-btn" style={{ right: 0 }}>
+      <button className="img-slider-btn" style={{ right: 0 }} onClick={showNextImage}>
         <ArrowBigRight />
       </button>
     </div>
